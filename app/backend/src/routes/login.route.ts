bd4a2d28f4a03fa4e74e61d8b5d55804e412d2c3
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-03-12 17:33:49 -0300

    errando como sempreparte II

--- app/backend/src/routes/login.route.ts
+++ app/backend/src/routes/login.route.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import UsersController from '../controller/UsersController';
-import UserMiddleware from '../middleware/userMiddleware';
+// import UserMiddleware from '../middleware/userMiddleware';
 
 const usersController = new UsersController();
-const userMiddleware = new UserMiddleware();
+// const userMiddleware = new UserMiddleware();
 
 const router = Router();
 
-router.post('/', userMiddleware. (req, res) => usersController.getUser(req, res));
+router.post('/', (req, res) => usersController.getUser(req, res));
 // router.post('login/role', (req, res) => usersController.getUserRole(req, res));
 
 export default router;
